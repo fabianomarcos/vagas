417c5553b404ade1b06fdc64ae5ce6a3502e5ac9
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-06-14 11:32:10 -0300

    refactor: improvement counter route

--- teste5.js
+++ teste5.js
@@ -1,9 +1,26 @@
+const axios = require('axios');
+const { baseURL } = require('./src/utils/constants');
+const { AppError } = require('./src/errors/AppError');
 
+module.exports = async function(req, res){
+    try {
+        const { name } =  req.body;
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+        if(!name) throw AppError('It is necessary to send the name', 400)
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+        const username = name.toLowerCase()
 
+        const { data } = await axios.get(baseURL)
+
+        const usersFounded = data.filter(user => user.name.toLowerCase().includes(username))
+
+        if(usersFounded.length == 0) return res.status(400).json({ error: 'No user found' })
+
+        const message = usersFounded.length > 1 ? "vezes" : "vez"
+        res.status(200).json({
+            message: `Usuário ${name} foi lido ${usersFounded.length} ${message}.`
+        });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
