436e799f921eaffc8f3579f102782e86bd38155d
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-06-14 11:28:04 -0300

    fix/refactor: fix variable name and improvement route

--- teste2.js
+++ teste2.js
@@ -1,17 +1,19 @@
-var data =  require("./fakeData");
+const axios = require('axios');
+const { baseURL } = require('./src/utils/constants');
+const { AppError } = require("./src/errors/AppError");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
+module.exports = async function(req, res){
+    try {
+        const { name, job } =  req.body;
+
+        if(!name || !job) throw new AppError("It is necessary to send the name and the job")
+
+        const newUser = { name, job }
+
+        await axios.post(baseURL, newUser)
+        res.status(201).json({ user: newUser });
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-
-    data.push(newUser)
-    
-    res.send(newUser);
-
 };
